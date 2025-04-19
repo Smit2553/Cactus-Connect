23b9958fd71a12c071787c3a7ad8a495284a9868
Smit Devrukhkar <smitdev3@gmail.com>
2025-04-18 17:18:00 -0700

    Initial Commit

---
+++ Cactus Connect/Cactus_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cactus_ConnectApp.swift
+//  Cactus Connect
+//
+//  Created by Smit Devrukhkar on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Cactus_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
