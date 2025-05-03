e66d3d78f33ec711003c821df579829f1ae5a1b7
Smit Devrukhkar <smitdev3@gmail.com>
2025-05-02 18:37:36 -0700

    Add initial EventDetails view with placeholder text

---
+++ Cactus Connect/EventDetails.swift
@@ -0,0 +1,18 @@
+//
+//  EventDetails.swift
+//  Cactus Connect
+//
+//  Created by Smit Devrukhkar on 4/29/25.
+//
+
+import SwiftUI
+
+struct EventDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventDetails()
+}
